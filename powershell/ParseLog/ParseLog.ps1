560222c37a79940d4b30c81eafb0c7f7cd2fc2ca
Gianni Bragante <gbragante@gmail.com>
2011-06-22 18:09:56 +0200

    20110622

--- powershell/ParseLog/ParseLog.ps1
+++ powershell/ParseLog/ParseLog.ps1
@@ -1,4 +1,4 @@
-﻿param([string]$LogName = ".", [string]$DateFormat, [string]$MatchText, [Boolean]$ShouldMatch = $true, [int]$MinutesBack, [string]$Tag, [string]$Name)
+﻿param([string]$LogName = ".", [string]$DateFormat, [string]$MatchText, [Boolean]$ShouldMatch = $true, [string]$Tag, [string]$Name, [boolean]$OnlyNewLines = $true, [int]$SleepMinutes = 5)
 
 function FindMonitor([string] $name) {
   foreach ($node in $monitors.monitors.childnodes) {
@@ -8,7 +8,33 @@ function FindMonitor([string] $name) {
   }
 }
 
+function ShowMsg($msg) {
+  $msg = (Get-Date).ToString("yyyyMMdd HH:mm:ss") + " " + $msg
+  Write-Host $msg
+}
+
 function AddResult() {
+  $apiKey = "3TE4K9JAJICQDKRANI3IPTBLQK"
+  $secretKey = "5CK8P2176A21Q9ARDDRGUJSAK"
+
+  #Requesting token
+  $url = "http://www.monitis.com/api?action=authToken&apikey=" + $apiKey + "&secretkey=" + $secretKey
+  $wc = new-object net.webclient
+  $resp = $wc.DownloadString($url).ToString()
+  $pos = $resp.IndexOf(":") + 2
+  $token = $resp.Substring($pos, $resp.Length - $pos - 2)
+
+  #Requests the monitor list in order to find the MonitorID
+  $url = 'http://www.monitis.com/customMonitorApi?action=getMonitors&apikey=' + $apiKey + "&tag=" + $Tag + "&output=xml"
+  $wc = new-object net.webclient
+  $resp = $wc.DownloadString($url).ToString()
+
+  $monitors = new-object "System.Xml.XmlDocument"
+  $monitors.LoadXml($resp)
+
+  $monitorID = FindMonitor $Name
+  $results = "match:" + $matchRes + ";"
+
   $nvc = new-object System.Collections.Specialized.NameValueCollection
   $nvc.Add('apikey', $apikey)
   $nvc.Add('validation', 'token')
@@ -22,61 +48,83 @@ function AddResult() {
   $wc = new-object net.webclient
   $wc.Headers.Add("Content-Type", "application/x-www-form-urlencoded")
   $resp = $wc.UploadValues('http://www.monitis.com/customMonitorApi', $nvc)
-  [text.encoding]::ascii.getstring($resp)
+  ShowMsg ([text.encoding]::ascii.getstring($resp))
 }
 
-$files = @(Get-ChildItem $LogName | Sort-Object LastWriteTime -descending)
-$LogName = $files[0].FullName
-Write-host "Parsing file:" $LogName
+$prevFile = ""
+$linesProc = 0 
 $dfre = [regex]("(?<date>" +  ($DateFormat -replace "\w", "\d") +")")
-$matchRes = 0;
-foreach ($line in Get-Content $LogName) {
-  try {
-    $dtLine = [datetime]::ParseExact($dfre.matches($line)[0].Value, $DateFormat, $null)
-    $diff = New-TimeSpan -Start $dtLine -End (Get-Date)
+ShowMsg "Starting"
+while ($true) {
+  $files = @(Get-ChildItem $LogName | Sort-Object LastWriteTime -descending)
+  $fileName = $files[0].FullName
+  ShowMsg ("Parsing file:" + $fileName )
   
-    if ($diff.TotalMinutes -le $MinutesBack) {
+  if ($prevFile -ne "") {
+    if ($fileName -ne $prevFile) {
+	  $linesProc = 0
+	  ShowMsg ("Log file name changed, parsing from start")
+    }
+	if ((Get-ChildItem $fileName).length -lt $prevSize) {
+	  $linesProc = 0
+	  ShowMsg ("Log file size changed, parsing from start")
+	}
+  }
+  $prevFile = $fileName
+  $prevSize = (Get-ChildItem $fileName).length
+  
+  $matchRes = 0;
+  $file = [System.io.File]::Open($fileName, 'Open', 'Read', 'ReadWrite')
+  $file = New-Object System.IO.StreamReader($file)
+  
+  if ($OnlyNewLines -eq $true) {
+    ShowMsg "Skipping to end of file"
+    while (-not $file.EndOfStream) {
+	  $line = $file.ReadLine()
+	  $linesProc++
+	}
+    $OnlyNewLines = $false;
+  } else {
+    ShowMsg ("Skipping " + $linesProc + " lines")
+    for ($l=1; $l -le $linesProc; $l++) {
+	  $line = $file.ReadLine()
+	}
+  }
+
+  $newLines = 0
+  if ($file.EndOfStream) {
+    Showmsg	"At end of file"
+  } else {
+    ShowMsg ("Parsing file")
+  }
+  while (-not $file.EndOfStream) {
+    $line = $file.ReadLine()
+	$newLines++
+    try {
       if ($line -match $MatchText) {
         $matchRes = 1;
         Write-Host $line
         if ($ShouldMatch -eq $true) {
-		  break;
-		}
-	  }
+          break;
+        }
+      }
+    }
+    catch {
     }
   }
-  catch {
-    Write-Host "Unk"
+  $file.close()
+  if ($ShouldMatch -eq $false) {
+    if ($matchRes -eq 0) {
+      $matchRes = 1;
+    } else {
+      $matchRes = 0;
+    }
   }
+  ShowMsg ("New lines processed: " + $newLines + ", sleeping " + $SleepMinutes + " minutes")
+  $linesProc = $linesProc + $newLines
+
+  ShowMsg ("Result: " + $matchRes)
+  AddResult
+  
+  Start-Sleep -s ($SleepMinutes * 60)
 }
-if ($ShouldMatch -eq $false) {
-  if ($matchRes -eq 0) {
-    $matchRes = 1;
-  } else {
-    $matchRes = 0;
-  }
-}
-
-Write-Host $matchRes
-
-$apiKey = "Your API key here"
-$secretKey = "Your Secret key here"
-
-#Requesting token
-$url = "http://www.monitis.com/api?action=authToken&apikey=" + $apiKey + "&secretkey=" + $secretKey
-$wc = new-object net.webclient
-$resp = $wc.DownloadString($url).ToString()
-$pos = $resp.IndexOf(":") + 2
-$token = $resp.Substring($pos, $resp.Length - $pos - 2)
-
-#Requests the monitor list in order to find the MonitorID
-$url = 'http://www.monitis.com/customMonitorApi?action=getMonitors&apikey=' + $apiKey + "&tag=" + $Tag + "&output=xml"
-$wc = new-object net.webclient
-$resp = $wc.DownloadString($url).ToString()
-
-$monitors = new-object "System.Xml.XmlDocument"
-$monitors.LoadXml($resp)
-
-$monitorID = FindMonitor $Name
-$results = "match:" + $matchRes + ";"
-AddResult
